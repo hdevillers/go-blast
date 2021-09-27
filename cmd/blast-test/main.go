f0c6745833a2b8601ef1fa3856dd05b7fe01a8a8
Hugo Devillers <hugo.devillers@gmail.com>
2021-09-27 17:12:51 +0200

    Testing script to run blast.

--- cmd/blast-test/main.go
+++ cmd/blast-test/main.go
@@ -1,26 +1,47 @@
 package main
 
 import (
-	"encoding/xml"
+	"flag"
 	"fmt"
-	"io/ioutil"
-	"os"
 
 	"github.com/hdevillers/go-blast"
+	"github.com/hdevillers/go-seq/seqio"
 )
 
 func main() {
-	f, e := os.Open("test.xml")
-	if e != nil {
-		panic(e)
+	query := flag.String("query", "", "Input query file.")
+	db := flag.String("db", "", "BLAST Database path.")
+	format := flag.String("format", "fasta", "Input query format.")
+	flag.Parse()
+
+	if *query == "" {
+		panic("You must provide an input query file.")
 	}
-	defer f.Close()
 
-	x, e := ioutil.ReadAll(f)
+	if *db == "" {
+		panic("You must provide an BLASTable database.")
+	}
 
-	var bo blast.BlastOutput
+	// Init blast object
+	blast := blast.NewBlast()
+	blast.Db = *db
 
-	xml.Unmarshal(x, &bo)
+	//blast.Par.SetOutput("test.blast")
 
-	fmt.Println(bo)
+	// Load query files
+	reader := seqio.NewReader(*query, *format, false)
+	reader.CheckPanic()
+	defer reader.Close()
+
+	for reader.Next() {
+		reader.CheckPanic()
+		blast.AddQuery(reader.Seq())
+	}
+
+	err := blast.Search()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(len(blast.Rst.Iterations[0].Hits))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hdevillers/go-blast
 
 go 1.15
+
+require github.com/hdevillers/go-seq v0.0.0-20210924111036-c52007c92af4
