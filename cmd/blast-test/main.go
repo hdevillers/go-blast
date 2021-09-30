d6cc3886ec463bd5b1a31a7064902c62587808f4
Hugo Devillers <hugo.devillers@gmail.com>
2021-09-30 18:18:01 +0200

    Control blast types and tasks.

--- cmd/blast-test/main.go
+++ cmd/blast-test/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	"github.com/hdevillers/go-blast"
 	"github.com/hdevillers/go-seq/seqio"
@@ -10,11 +12,33 @@ import (
 func main() {
 	query := flag.String("query", "", "Input query file.")
 	format := flag.String("format", "fasta", "Input query format.")
-	db := flag.String("db", "", "BLAST Database path.")
+	db := flag.String("db", "", "BLAST database path or FASTA database.")
 	header := flag.String("header", "", "Required header for the summary.")
 	maxHit := flag.Int("max-hits", 100, "Maximal number of hit to report.")
+	tool := flag.String("tool", "blastp", "BLAST+ tool to use.")
+	task := flag.String("task", "", "Specify the BLAST+ task to run (default = same as tool).")
+	showTool := flag.Bool("tool-list", false, "Display the list of available tools.")
 
 	flag.Parse()
+
+	allowedTools := map[string]string{}
+	allowedTools["blastn"] = "Nucl => Nucl comparison."
+	allowedTools["blastp"] = "Prot => Prot comparison."
+	allowedTools["tblastn"] = "Prot=>Trans(Nucl) comparison."
+	allowedTools["blastx"] = "Trans(Nucl)=>Prot comparison."
+	allowedTools["tblastx"] = "Trans(Nucl)=>Trans(Nucl) comparison."
+
+	if *showTool {
+		for k, v := range allowedTools {
+			fmt.Printf("%s\t%s\n", k, v)
+		}
+		os.Exit(1)
+	}
+
+	// Check tool value
+	if _, ok := allowedTools[*tool]; !ok {
+		panic(fmt.Sprintf("The tool %s does not exist!", *tool))
+	}
 
 	if *query == "" {
 		panic("You must provide an input query file.")
@@ -27,6 +51,14 @@ func main() {
 	// Init blast object
 	blast := blast.NewBlast()
 	blast.Db = *db
+
+	// Setup argument values
+	blast.Par.SetTool(*tool)
+	if *task == "" {
+		// Default task is the tool
+		*task = *tool
+	}
+	blast.Par.SetTask(*task)
 
 	// Load query files
 	reader := seqio.NewReader(*query, *format, false)
--- param.go
+++ param.go
@@ -16,26 +16,36 @@ const (
 
 type Param struct {
 	tool    string
+	task    string
 	evalue  float64
 	threads int
 	outfmt  string
 	output  string
+	chkTask map[string]map[string]int
 }
 
 func NewParam() *Param {
 	var p Param
 	p.tool = D_TOOL
+	p.task = D_TOOL // Default task is the tool itself
 	p.evalue = D_EVALUE
 	p.threads = D_THREADS
 	p.outfmt = D_OUTFMT
 	p.output = D_OUTPUT
-
+	p.chkTask = make(map[string]map[string]int)
+	p.chkTask["blastn"] = map[string]int{"blastn": 1, "megablast": 1, "dc-megablast": 1, "blastn-short": 1}
+	p.chkTask["blastp"] = map[string]int{"blastp": 1, "blastp-short": 1, "blastp-fast": 1}
+	p.chkTask["tblastn"] = map[string]int{"tblastn": 1, "tblastn-fast": 1}
+	p.chkTask["blastx"] = map[string]int{"blastx": 1, "blastx-fast": 1}
 	return &p
 }
 
 // SETTER
 func (p *Param) SetTool(t string) {
 	p.tool = t
+}
+func (p *Param) SetTask(t string) {
+	p.task = t
 }
 func (p *Param) SetEvalue(e float64) {
 	p.evalue = e
@@ -54,6 +64,9 @@ func (p *Param) SetOutput(o string) {
 func (p *Param) GetTool() string {
 	return p.tool
 }
+func (p *Param) GetTask() string {
+	return p.task
+}
 func (p *Param) GetEvalue() float64 {
 	return p.evalue
 }
@@ -65,6 +78,11 @@ func (p *Param) GetOutfmt() string {
 }
 func (p *Param) GetOutput() string {
 	return p.output
+}
+
+func (p *Param) CheckTask() bool {
+	_, test := p.chkTask[p.tool][p.task]
+	return test
 }
 
 // GENERATE THE COMMAND LINE
@@ -85,6 +103,15 @@ func (p *Param) GetCmd(db string) *exec.Cmd {
 		cmd.Args = append(cmd.Args, "-db", db)
 	} else {
 		cmd.Args = append(cmd.Args, "-subject", db)
+	}
+	// No task for tblastx
+	if p.tool != "tblastx" {
+		// Check task
+		if p.CheckTask() {
+			cmd.Args = append(cmd.Args, "-task", p.task)
+		} else {
+			panic(fmt.Sprintf("The task %s is not available for the tool %s.", p.task, p.tool))
+		}
 	}
 
 	return cmd
